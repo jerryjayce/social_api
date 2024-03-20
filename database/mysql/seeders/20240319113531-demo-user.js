206795651b3313867616d984f0097c15d45ea31a
jerryjayce <jerryjayce@gmail.com>
2024-03-20 19:26:00 +0100

    added password encryption to user seed

--- database/mysql/seeders/20240319113531-demo-user.js
+++ database/mysql/seeders/20240319113531-demo-user.js
@@ -3,12 +3,18 @@
 /** @type {import("sequelize-cli").Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
+
+        let bcrypt = require("bcrypt");
+        const salt_rounds = 10;
+        const password = "12345678";
+
+
         await queryInterface.bulkInsert("users", [
             {
                 full_name: "John Doe",
                 age: 30,
                 email: "john@example.com",
-                password: "password123",
+                password: await bcrypt.hash(password, salt_rounds),
                 phone: "123-456-7890",
                 status: "active",
                 createdAt: new Date(),
@@ -18,7 +24,7 @@ module.exports = {
                 full_name: "Jane Smith",
                 age: 25,
                 email: "jane@example.com",
-                password: "password456",
+                password: await bcrypt.hash(password, salt_rounds),
                 phone: "987-654-3210",
                 status: "inactive",
                 createdAt: new Date(),
@@ -28,6 +34,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete('users', null, {});
+        await queryInterface.bulkDelete("users", null, {});
     }
 };
