47fbd6383128e21d79d5c1178c4611c1fe00847d
jerryjayce <jerryjayce@gmail.com>
2024-03-20 11:54:29 +0100

    class validator error formatter

---
+++ src/utils/ClassValidatorErrorFormatter.ts
@@ -0,0 +1,10 @@
+
+export const class_validator_error_formatter = (errors) => {
+    const errors_response = errors.map((error) => ({
+        field: error.property,
+        value: error.value,
+        message: error.constraints[Object.keys(error.constraints)[0]],
+    }));
+
+    return { errors: errors_response };
+};
